e0ca4ca9639c1ab4341c76f8c11b786ef3d37206
Kévin <redfoox84@gmail.com>
2019-10-02 14:52:20 +0200

    Création du background "UnderConstruct"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as Particles from 'particlesjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StarbreakersNet-Website';
+  ngOnInit() {
+    Particles.init({
+      selector: '.constructionBackground',
+      maxParticles: 150,
+      sizeVariations: 3,
+      speed: 0.5,
+      color: ['#FAED4F', '#8D1C1A'],
+      minDistance: 120,
+      connectParticles: true,
+      responsive: [
+        {
+          breakpoint: 1000,
+          options: {
+            maxParticles: 50
+          }
+        },
+        {
+          breakpoint: 500,
+          options: {
+            maxParticles: 25
+          }
+        }
+      ]
+    });
+  }
 }
