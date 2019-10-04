b8747222ca241001261325bea60f3c55a0386a72
Kévin <redfoox84@gmail.com>
2019-10-04 18:19:41 +0200

    Comportement de la navigation fonctionnel
    Il restera cependant le style à faire puis convertir le slider avec les méthodes d'Angular et Fomantic-UI

--- src/app/home-screen/slider-nav/slider-nav.component.ts
+++ src/app/home-screen/slider-nav/slider-nav.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderNavComponent implements OnInit {
 
+  elements = [
+    {
+      name: 'Accueil',
+      src: '/assets/icons/icon_strb_active.png',
+      color: '#8D1C1A'
+    },
+    {
+      name: 'Heroes of the Storm',
+      src: '/assets/icons/icon_heroes_active.png',
+      color: '#8D1C1A'
+    },
+    {
+      name: 'World of Warcraft',
+      src: '/assets/icons/icon_wow_active.png',
+      color: '#8D1C1A'
+    },
+    {
+      name: 'League of Legens',
+      src: '/assets/icons/icon_lol_active.png',
+      color: '#8D1C1A'
+    },
+    {
+      name: 'Grand Theft Auto V',
+      src: '/assets/icons/icon_gtav_active.png',
+      color: '#8D1C1A'
+    },
+    {
+      name: 'Counter Striker: GO',
+      src: '/assets/icons/icon_csgo_active.png',
+      color: '#8D1C1A'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
