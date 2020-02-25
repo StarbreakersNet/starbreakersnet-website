f9e2f64eb8b64dea304eada6a559bd48f9f27ec0
Kévin <anais.kevin@me.com>
2020-02-25 18:28:07 +0100

    Génération et création du composant feedback
    Ajout d'un bouton feedback pour directement signaler un problème vers le GitLab pour l'issue #9
    Ajout d'un numéro de version du site

---
+++ src/app/developer/feedback/feedback.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.sass']
+})
+export class FeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    jQuery('.feedback-btn .icon.button').popup({
+      position: 'left center',
+      content: 'Signaler un problème'
+    });
+  }
+
+  navigateToBugTrack() {
+    window.location.href = 'https://gitlab.com/starbreakersdevteam/starbreakersnet-website/issues/new/';
+  }
+}
