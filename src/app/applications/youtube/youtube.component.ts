a5f036297c80399b8cf936f2f3f2e5c7e7c3d392
TheRedfoox <anais.kevin@me.com>
2020-03-27 18:36:43 +0100

    WIP: Ajout de l'iframe de la playlist du Zap des Starbreakers

--- src/app/applications/youtube/youtube.component.ts
+++ src/app/applications/youtube/youtube.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NotificationService} from '../../services/notification/notification.service';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-youtube',
@@ -8,9 +9,13 @@ import {NotificationService} from '../../services/notification/notification.serv
 })
 export class YoutubeComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://www.youtube.com/embed/videoseries?list=PLRyncVRdYNSJIUIHfr8f5Tewxmab2BtIj';
+  urlSafe: SafeResourceUrl;
+
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
     NotificationService.notify('Youtube works !');
   }
 
