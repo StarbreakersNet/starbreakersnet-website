b6a6a37c3f18404f680f1fa32dbd5dc44635ea68
TheRedfoox <anais.kevin@me.com>
2021-01-01 19:04:43 +0100

    Changement du background et size

--- src/app/elements/particles-canva/particles-canva.component.ts
+++ src/app/elements/particles-canva/particles-canva.component.ts
@@ -11,112 +11,61 @@ export class ParticlesCanvaComponent implements OnInit {
 
   particlesOptions = {
     fpsLimit: 60,
+    interactivity: {
+      detectsOn: 'canvas',
+      events: {
+        onClick: {
+          enable: false,
+          mode: 'push'
+        },
+        onHover: {
+          enable: false,
+          mode: 'repulse'
+        },
+        resize: false
+      }
+    },
     particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      },
       color: {
         value: ['#8D1C1A', '#FAED4F']
       },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#000000'
-        },
-        polygon: {
-          nb_sides: 5
-        },
-        image: {
-          src: 'img/github.svg',
-          width: 100,
-          height: 100
-        }
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
-        }
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
+      links: {
         color: 'random',
+        distance: 150,
+        enable: true,
         opacity: 0.4,
         width: 1
       },
-      move: {
-        enable: true,
-        speed: 2,
-        direction: 'none',
-        random: false,
-        straight: false,
-        out_mode: 'out',
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
-        }
-      }
-    },
-    interactivity: {
-      detect_on: 'canvas',
-      events: {
-        onhover: {
-          enable: false
-        },
-        onclick: {
-          enable: false
-        },
-        resize: true
+      collisions: {
+        enable: true
       },
-      modes: {
-        grab: {
-          distance: 140,
-          line_linked: {
-            opacity: 1
-          }
+      move: {
+        direction: 'top',
+        enable: true,
+        outMode: 'bounce',
+        random: true,
+        speed: 1,
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800
         },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4
-        },
-        push: {
-          particles_nb: 4
-        },
-        remove: {
-          particles_nb: 2
-        }
+        value: 70
+      },
+      opacity: {
+        value: 0.5
+      },
+      shape: {
+        type: 'circle'
+      },
+      size: {
+        random: true,
+        value: 3
       }
     },
-    retina_detect: true
+    detectRetina: true
   };
 
   particlesLoaded(container): void {
