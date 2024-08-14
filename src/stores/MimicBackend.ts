b467ed993dcb50f49b26b3383b3dc9101b855f34
Kévin ANAIS <anais.kevin@me.com>
2024-08-14 15:44:48 +0000

    Refonte complète en Vue3 et NaiveUI

---
+++ src/stores/MimicBackend.ts
@@ -0,0 +1,8 @@
+export default {
+  getViewerDiscordUrl: () => {
+    return 'https://discordapp.com/widget?id=674668711566639114&theme=dark';
+  },
+  getViewerYoutubeZapUrl: () => {
+    return 'https://www.youtube.com/embed/videoseries?list=PLRyncVRdYNSJIUIHfr8f5Tewxmab2BtIj';
+  }
+}
