e6c52386997caeb43263f15649b1ae22ba11c429
Kévin ANAIS <anais.kevin@me.com>
2024-08-14 15:44:48 +0000

    Refonte complète en Vue3 et NaiveUI

---
+++ src/AppUtils.ts
@@ -0,0 +1,53 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { darkTheme, lightTheme, NIcon } from "naive-ui";
+import { h } from "vue";
+import { naiveDark, naiveLight } from "@/assets/themes/naiveTheme";
+
+type themeType = "dark" | "light" | "system";
+type themeNaiveType = "naive" | "naiveOverride";
+
+const themeMappings = {
+  dark: { naive: naiveDark, naiveOverride: darkTheme },
+  light: { naive: naiveLight, naiveOverride: lightTheme },
+};
+
+export function renderFontAwesomeIcon(option: { fas?: string, far?: string, fab?: string }) {
+  if (option.fas) {
+    return h(NIcon, null, () => h(FontAwesomeIcon, { icon: ["fas", option.fas] }));
+  } else if (option.far) {
+    return h(NIcon, null, () => h(FontAwesomeIcon, { icon: ["far", option.far] }));
+  } else if (option.fab) {
+    return h(NIcon, null, () => h(FontAwesomeIcon, { icon: ["fab", option.fab] }));
+  } else {
+    return null;
+  }
+}
+
+export function preferedOsTheme() {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
+
+export function getTheme(theme: themeType, type: themeNaiveType) {
+  const osTheme = preferedOsTheme();
+
+  if (theme === "system" || !theme) {
+    theme = osTheme;
+  }
+
+  return themeMappings[theme][type] ?? themeMappings.dark[type];
+}
+
+export function getNaiveTheme(theme: themeType) {
+  return getTheme(theme, "naiveOverride");
+}
+
+export function getNaiveOverrideTheme(theme: themeType) {
+  return getTheme(theme, "naive");
+}
+
+export default {
+  renderFontAwesomeIcon,
+  getNaiveTheme,
+  getNaiveOverrideTheme,
+  preferedOsTheme,
+};
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const routes = [
+  { name: "home", label: "Accueil" },
+  { name: "discord", label: "Discord" },
+  { name: "youtube", label: "Le Zap" },
+];
+</script>
+
+<template>
+  <n-flex align="center" justify="center" class="component">
+    <div v-for="route in routes" class="item">
+      <router-link :to="{name: route.name}">
+        <n-button>
+          {{ route.label }}
+        </n-button>
+      </router-link>
+
+    </div>
+  </n-flex>
+</template>
+
+<style lang="sass" scoped>
+@import @/assets/variables.sass
+
+.component
+  padding: 1em
+  margin: unset
+  border: 1px solid rgba(255, 255, 255, 0.18)
+  border-radius: 16px
+  background: rgba(83, 83, 83, 0.25)
+  backdrop-filter: blur(13px)
+
+  .item
+    padding: 1em .75em
+
+    button
+      width: 10em
+      aspect-ratio: 16 / 9
+      border-radius: $sn-main-border-radius
+      transition: transform .1s ease-in-out
+
+      &:hover
+        transform: scale(1.1)
+
+      &:active
+        transform: scale(1)
+
+</style>
