00c5b21d6db974e4f4daf5ef002fc5a5471c98c8
Kévin ANAIS <anais.kevin@me.com>
2024-09-08 22:59:18 +0000

    Update 3.1 Conversion de tout le site en Vue

---
+++ src/components/AppVersion.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { version } from "@/../package.json";
+
+const packageVersion = ref(version);
+</script>
+
+<template>
+  <n-flex :size="0" align="center" justify="center">
+    v{{ packageVersion }}
+  </n-flex>
+</template>
+
+<style scoped lang="sass">
+
+</style>
--- src/components/ViewerYoutube.vue
+++ src/components/ViewerYoutube.vue
@@ -1,18 +1,28 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref } from "vue";
 import MimicBackend from "@/stores/MimicBackend";
+import AppContainer from "@/components/AppContainer.vue";
+
 const url: string = MimicBackend.getViewerYoutubeZapUrl();
+const isIframeLoaded = ref(false);
 </script>
 
 <template>
-  <iframe
-    :src="url"
-    allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen/>
+  <app-container :loaded="isIframeLoaded" app="youtube">
+    <iframe
+      :src="url"
+      allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      @load="isIframeLoaded = true" />
+  </app-container>
 </template>
 
-<style scoped lang="sass">
+<style lang="sass" scoped>
+@import @/assets/variables.sass
+
 iframe
   width: 100%
   height: 100%
   border: none
+  border-radius: $sn-apple-border-radius
 </style>
